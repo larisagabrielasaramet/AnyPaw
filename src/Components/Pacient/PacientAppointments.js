86db72a5e0c411690cfaed829cddea01d8ac8f23
Larisa <sarametlarisa@yahoo.com>
2024-06-01 22:51:47 +0300

    appointment schedule

--- src/Components/Pacient/PacientAppointments.js
+++ src/Components/Pacient/PacientAppointments.js
@@ -4,6 +4,7 @@ import moment from "moment/moment";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { FIREBASE_DB } from "../../firebase/firebase";
 import { getAuth } from "firebase/auth";
+import { Timestamp } from "firebase/firestore";
 
 function AppointmentPage() {
   const [week, setWeek] = useState(0);
@@ -11,6 +12,18 @@ function AppointmentPage() {
   const [doctors, setDoctors] = useState([]);
   const hours = Array.from({ length: 9 }, (_, i) => i + 9);
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  const [availability, setAvailability] = useState({});
+
+  // useEffect(() => {
+  //   const newAvailability = {};
+  //   days.forEach((day) => {
+  //     newAvailability[day] = {};
+  //     hours.forEach((hour) => {
+  //       newAvailability[day][hour] = checkDoctorAvailability(day, hour);
+  //     });
+  //   });
+  //   setAvailability(newAvailability);
+  // }, [appointments, doctors]);
 
   useEffect(() => {
     const fetchAppointments = async () => {
@@ -19,7 +32,16 @@ function AppointmentPage() {
       const appointmentsList = appointmentsSnapshot.docs.map((doc) =>
         doc.data()
       );
-      setAppointments(appointmentsList);
+
+      const appointmentsByDoctor = {};
+      appointmentsList.forEach((appointment) => {
+        if (!appointmentsByDoctor[appointment.userId]) {
+          appointmentsByDoctor[appointment.userId] = [];
+        }
+        appointmentsByDoctor[appointment.userId].push(appointment);
+      });
+      console.log("---->>>>", appointmentsByDoctor);
+      setAppointments(appointmentsByDoctor);
     };
 
     const fetchDoctors = async () => {
@@ -35,24 +57,93 @@ function AppointmentPage() {
     fetchDoctors();
   }, []);
 
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [selectedHour, setSelectedHour] = useState(null);
+
+  useEffect(() => {
+    checkDoctorAvailability(selectedDay, selectedHour);
+  }, [selectedDay, selectedHour]);
+
   const checkDoctorAvailability = (day, hour) => {
-    const selectedTime = moment(`${day} ${hour}:00`, "YYYY-MM-DD HH:mm");
+    console.log("Checking doctor availability for ", day, hour);
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const moment = require("moment");
+    const today = new Date();
+    const currentDayOfWeek = today.getDay();
+    const distance = (days.indexOf(day) - currentDayOfWeek + 7) % 7;
+    const selectedDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + distance
+    );
+    const formattedDate = `${selectedDate.getFullYear()}-${
+      selectedDate.getMonth() + 1
+    }-${selectedDate.getDate()}`;
+    console.log(`Formatted date: ${formattedDate}`);
+    const selectedTime = moment(
+      `${formattedDate} ${hour}:00`,
+      "YYYY-MM-DD HH:mm"
+    );
+    console.log(
+      `Selected time: ${selectedTime.format(
+        "MMMM D, YYYY [at] hh:mm:ss A [UTC]Z"
+      )}`
+    );
+
+    console.log("Doctors: ", doctors);
     const availableDoctors = doctors.filter((doctor) => {
-      const doctorAppointments = appointments.filter((appointment) => {
-        if (appointment && appointment.timestamp) {
-          const appointmentTime = moment(appointment.timestamp.toDate());
-          return (
-            appointment.userId === doctor.id &&
-            appointmentTime.isSame(selectedTime, "hour")
-          );
+      const doctorAppointments = appointments[doctor.uid] || [];
+      console.log("Doctor appointments: ", doctorAppointments);
+      const hasAppointmentAtSameTime = doctorAppointments.some(
+        (appointment) => {
+          console.log("++++++++++++", appointment);
+          if (appointment && appointment.date) {
+            const appointmentTime = appointment.date.toDate();
+            const options = {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+              second: "numeric",
+              hour12: true,
+              timeZone: "Etc/GMT-3",
+            };
+            console.log("!!!!!!!!!!!", appointmentTime);
+            const selectedTimeString = selectedTime.toLocaleString(
+              "en-US",
+              options
+            );
+            return (
+              appointmentTime.toLocaleString("en-US", options) ===
+              selectedTimeString
+            );
+          }
+          return false;
         }
-        return false;
-      });
-      return doctorAppointments.length === 0;
+      );
+
+      return !hasAppointmentAtSameTime;
     });
+
+    if (availableDoctors.length === 0) {
+      console.log(`No available doctors on ${day} at ${hour}:00`);
+    }
+
+    console.log(
+      `Available doctors: ${JSON.stringify(availableDoctors, null, 2)}`
+    );
+
     return availableDoctors;
   };
-
   moment.locale("en", {
     week: {
       dow: 1,
@@ -63,30 +154,29 @@ function AppointmentPage() {
     const auth = getAuth();
     const userId = auth.currentUser ? auth.currentUser.uid : null;
 
-    // Crează un nou appointment
     const newAppointment = {
-      day,
-      hour,
       doctorId,
       userId,
+      timestamp: Timestamp.fromDate(new Date(`2024-05-30T${hour}:00:00+03:00`)),
     };
 
     try {
-      // Adaugă noul appointment în Firestore
-      const appointmentsCollection = collection(FIREBASE_DB, "appointments");
+      const appointmentsCollection = collection(FIREBASE_DB, "dappointments");
       await addDoc(appointmentsCollection, newAppointment);
 
-      // Actualizează lista locală de appointments
-      setAppointments((prevAppointments) => [
-        ...prevAppointments,
-        newAppointment,
-      ]);
+      setAppointments((prevAppointments) => {
+        const newAppointments = { ...prevAppointments };
+        if (!newAppointments[doctorId]) {
+          newAppointments[doctorId] = [];
+        }
+        newAppointments[doctorId].push(newAppointment);
+        return newAppointments;
+      });
     } catch (error) {
       console.error("Error adding document: ", error);
-      // Handle the error appropriately here
+      alert("There was an error scheduling the appointment. Please try again.");
     }
   };
-
   const getFirstDayOfWeek = () => {
     const now = new Date();
     const dayOfWeek = now.getDay();
@@ -105,6 +195,7 @@ function AppointmentPage() {
               const currentDate = new Date(
                 getFirstDayOfWeek().getTime() + index * 24 * 60 * 60 * 1000
               );
+              // console.log("currentDate", currentDate);
               return (
                 <th key={day}>
                   {currentDate.toLocaleDateString()} <br />
@@ -118,23 +209,31 @@ function AppointmentPage() {
           {hours.map((hour) => (
             <tr key={hour}>
               <td>{hour}:00</td>
-              {days.map((day, index) => {
-                const availableDoctors = checkDoctorAvailability(day, hour);
+              {days.map((day) => {
+                const availableDoctors = availability[day]
+                  ? availability[day][hour] || []
+                  : [];
+                // console.log("availableDoctors", availableDoctors);
                 const hasAppointment = availableDoctors.length === 0;
                 return (
                   <td
                     key={day}
-                    className={hasAppointment ? styles.red : styles.green}
+                    // className={hasAppointment ? styles.noDoctors : ""}
                   >
                     {hasAppointment ? null : (
                       <select
-                        onChange={(event) =>
-                          handleSelectDoctor(day, hour, event)
-                        }
+                        onChange={(event) => {
+                          setSelectedDay(day);
+                          setSelectedHour(hour);
+                          handleSelectDoctor(day, hour, event);
+                        }}
                       >
-                        {availableDoctors.map((doctor) => (
-                          <option value={doctor.id}>{doctor.fullName}</option>
-                        ))}
+                        {availableDoctors.map((doctor) => {
+                          // console.log(doctor);
+                          return (
+                            <option value={doctor.id}>{doctor.fullName}</option>
+                          );
+                        })}
                       </select>
                     )}
                   </td>
