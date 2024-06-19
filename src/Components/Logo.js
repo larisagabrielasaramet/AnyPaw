d6a6e006c22a99e69240381d70161d0e957fed0a
Larisa <sarametlarisa@yahoo.com>
2024-06-19 10:53:50 +0300

    png to jpg

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.png";
+import logo from "./logo.jpg";
 function Logo() {
   return (
     <div style={{ position: "absolute", top: 12, left: 120 }}>
